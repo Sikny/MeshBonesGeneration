fb41f279cb46640224e38dc82c6f0d568765f369
Quentin <pamartqu@gmail.com>
2021-12-15 01:49:38 +0100

    assets + started script

---
+++ Assets/Scripts/SeparateBodyMeshParts.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeparateBodyMeshParts : MonoBehaviour
+{
+    public MeshFilter meshFilter;
+    public float errorParam = 0.1f;
+
+    private Vector3[] _vertices;
+    private void Awake()
+    {
+        Mesh sourceMesh = meshFilter.sharedMesh;
+        List<MeshFilter> outMeshes = new List<MeshFilter>();
+
+        _vertices = sourceMesh.vertices;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireMesh(meshFilter.sharedMesh);
+        
+        Gizmos.color = Color.black;
+        var vertices = meshFilter.sharedMesh.vertices;
+        for (int i = vertices.Length - 1; i >= 0; i--)
+        {
+            Vector3 vertex = vertices[i];
+            Gizmos.DrawSphere(vertex, 0.0025f);
+        }
+    }
+}
