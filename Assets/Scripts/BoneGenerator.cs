f2df8268b7269b1b5a55f18cb952ce555335c808
Quentin <pamartqu@gmail.com>
2022-01-17 18:24:32 +0100

    basic bone direction detection

--- Assets/Scripts/BoneGenerator.cs
+++ Assets/Scripts/BoneGenerator.cs
@@ -4,20 +4,53 @@ using UnityEngine;
 public class BoneGenerator : MonoBehaviour
 {
     public Mesh sourceMesh;
+    public GameObject vertexSpherePrefab;
+    public Vector3 baryCenter;
+    public Vector3 outBoneVector;
+
+    private void Start()
+    {
+        GenerateBone();
+    }
 
     [ContextMenu("Generate Bone")]
     public void GenerateBone()
     {
+        // 1 - barycentre
+        Vector3 g = Vector3.zero;
+        Vector3[] vertices = sourceMesh.vertices;
+        int vertexCount = vertices.Length;
+        for (int i = vertexCount - 1; i >= 0; --i)
+        {
+            Instantiate(vertexSpherePrefab, vertices[i], Quaternion.identity);
+            g += vertices[i];
+        }
+        g /= vertexCount;   // barycentre
+        baryCenter = g;
         
+        // 2 - mise a l origine du barycentre
+        Vector3 og = g;
+        for (int i = vertexCount - 1; i >= 0; i--)
+        {
+            vertices[i] -= og;
+        }
+        
+        // 3 - calcul matrice de covariance M : 3x3
+        var covarianceM = vertices.CovarianceMat();
+        
+        // 4 - algorithme de recherche de valeur propre dominante de M et de son vecteur propre v associé
+        var eigenVec = covarianceM.GetEigenVector(100);
+        outBoneVector = eigenVec;
+
+        // 5 - 
     }
 
     private void OnDrawGizmos()
     {
-        /*if (sourceMesh == null) return;
-        Gizmos.color = Color.blue;
+        Gizmos.color = Color.green;
         for (int i = sourceMesh.vertices.Length - 1; i >= 0; i--)
         {
-            Gizmos.DrawSphere(sourceMesh.vertices[i], 0.001f);
-        }*/
+            Gizmos.DrawRay(baryCenter, outBoneVector/2);
+        }
     }
 }
---
+++ Assets/Scripts/GeometryUtils.cs
@@ -0,0 +1,78 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public static class GeometryUtils
+{
+    public static float[][] CovarianceMat(this Vector3[] values)
+    {
+        var xList = values.Select(pos => pos.x).ToArray();
+        var yList = values.Select(pos => pos.y).ToArray();
+        var zList = values.Select(pos => pos.z).ToArray();
+        return new[]
+        {
+            new [] {xList.Variance(), Covariance(xList, yList), Covariance(xList, zList)},
+            new [] {Covariance(yList, xList), yList.Variance(), Covariance(yList, zList)},
+            new [] {Covariance(zList, xList), Covariance(zList, yList), zList.Variance()},
+        };
+    }
+
+    public static float Covariance(float[] v1, float[] v2)
+    {
+        return v1.Zip(v2, (a, b) => a * b).Sum() / v1.Length - v1.Mean() * v2.Mean();
+    }
+    
+    public static float Variance(this float[] values)
+    {
+        float mean = values.Mean();
+        return values.Sum(v => v * v)/values.Length - mean * mean;
+    }
+
+    public static float Mean(this float[] values)
+    {
+        return values.Sum() / values.Length;
+    }
+
+    public static Vector3 GetEigenVector(this float[][] matrix, int iterations)
+    {
+        // 1 - vecteur initial v0 dont la plus grande composante est 1
+        Vector3 v0 = Vector3.forward;
+        
+        // 2
+        Vector3 vK = v0;
+        for (int k = 0; k < iterations; ++k)
+        {
+            var prod = matrix.Multiply(vK);
+            var lambdaK = prod.GetGreatestAbsoluteValue();
+            vK = 1 / lambdaK * prod;
+        }
+        
+        // 3
+        return vK;
+    }
+
+    public static float GetGreatestAbsoluteValue(this Vector3 value)
+    {
+        var absX = Mathf.Abs(value.x);
+        var absY = Mathf.Abs(value.y);
+        var absZ = Mathf.Abs(value.z);
+
+        if (absX < absY)
+        {
+            if (absY < absZ) return value.z;
+
+            return value.y;
+        }
+        if (absX < absZ) return value.z;
+
+        return value.x;
+    }
+
+    public static Vector3 Multiply(this float[][] matrix, Vector3 vector)
+    {
+        return new Vector3(
+            matrix[0][0] * vector.x + matrix[0][1] * vector.y + matrix[0][2] * vector.z,
+            matrix[1][0] * vector.x + matrix[1][1] * vector.y + matrix[1][2] * vector.z,
+            matrix[2][0] * vector.x + matrix[2][1] * vector.y + matrix[2][2] * vector.z
+        );
+    }
+}
