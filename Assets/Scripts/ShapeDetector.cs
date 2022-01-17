dd2223ae2ab89d477a63ac9f47cd95a1918e5656
Quentin <pamartqu@gmail.com>
2022-01-17 05:47:40 +0100

    character splitted

--- Assets/Scripts/ShapeDetector.cs
+++ Assets/Scripts/ShapeDetector.cs
@@ -1,72 +1,79 @@
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
-public class ShapeDetector : MonoBehaviour {
-    private class Sphere {
-        public Vector3 Center;
-        public float Radius;
-    }
-    public float errorParam = 0.1f;
-    public float centerMinDistance = 0.1f;
-    public int minVerticesCountForShape = 10;
+public class ShapeDetector : MonoBehaviour
+{
     public MeshFilter meshFilter;
+    public float radiusError = 0.1f;
+    
+    private struct Sphere
+    {
+        public float Radius;
+        public Vector3 Center;
+    }
+
     private List<Sphere> _spheres = new List<Sphere>();
 
     [ContextMenu("Detect Spheres")]
-    private void DetectSpheres() {
-        _spheres.Clear();
-        Vector3[] vertices = meshFilter.sharedMesh.vertices;
-        int verticesCount = vertices.Length;
-        List<Vector3> spherePoints = new List<Vector3>();
-        for (int i = 0; i < verticesCount; ++i) {
-            Vector3 currentCenter = vertices[i];
-            spherePoints.Clear();
-
-            for (int j = 0; j < verticesCount; ++j) {
-                if (j == i) continue;
-                Vector3 currentVertex = vertices[j];
-                float dist = Vector3.Distance(currentCenter, currentVertex);
-                if (dist < errorParam) {
-                    spherePoints.Add(currentVertex);
-                }
+    private void DetectSpheres()
+    {
+        void CheckSphere(ref List<Vector3> pointsList)
+        {
+            Vector3 center = Vector3.zero;
+            for (int i = pointsList.Count - 1; i >= 0; --i)
+            {
+                center += pointsList[i];
             }
-
-            if (spherePoints.Count >= minVerticesCountForShape) {
-                currentCenter = Vector3.zero;
-                foreach (var spherePoint in spherePoints) {
-                    currentCenter += spherePoint;
+            center /= pointsList.Count;
+            float meanRadius = 0;
+            for (int i = pointsList.Count - 1; i >= 0; --i)
+            {
+                meanRadius += Vector3.Distance(center, pointsList[i]);
+            }
+            meanRadius /= pointsList.Count;
+            for (int i = pointsList.Count - 1; i >= 0; --i)
+            {
+                float dist = Vector3.Distance(center, pointsList[i]);
+                if (Mathf.Abs(dist - meanRadius) > radiusError)
+                {
+                    pointsList.RemoveAt(i);
+                    break;
                 }
-
-                currentCenter /= spherePoints.Count;
-                // check center is not too close to another
-                bool validCenter = true;
-                for (int k = _spheres.Count - 1; k >= 0; k--) {
-                    if (Vector3.Distance(_spheres[k].Center, currentCenter) < centerMinDistance) {
-                        validCenter = false;
-                        break;
-                    }
-                }
-                if (!validCenter) continue;
-                
-                float radius = 0;
-                for (int k = spherePoints.Count - 1; k >= 0; k--) {
-                    radius += Vector3.Distance(spherePoints[k], currentCenter);
-                }
-                radius /= spherePoints.Count;
-                Debug.Log("Added Sphere with center " + currentCenter + " and radius " + radius + " ; vertex count = " + spherePoints.Count);
-                _spheres.Add(new Sphere {Center = currentCenter, Radius = radius});
             }
         }
-        Debug.Log("Sphere count : " + _spheres.Count);
+
+        var vertices = meshFilter.sharedMesh.vertices;
+        var pointsList = new List<Vector3>();
+        for (int i = vertices.Length - 1; i >= 0; i--)
+        {
+            pointsList.Add(vertices[i]);
+            CheckSphere(ref pointsList);
+        }
+        Debug.Log(pointsList.Count);
+        
+        _spheres.Clear();
+        Vector3 baryCenter = Vector3.zero;
+        for (int i = pointsList.Count - 1; i >= 0; --i)
+        {
+            baryCenter += pointsList[i];
+        }
+        baryCenter /= pointsList.Count;
+        float meanRadius = 0;
+        for (int i = pointsList.Count - 1; i >= 0; --i)
+        {
+            meanRadius += Vector3.Distance(baryCenter, pointsList[i]);
+        }
+        meanRadius /= pointsList.Count;
+        _spheres.Add(new Sphere{Center = baryCenter, Radius = meanRadius});
+        Debug.Log("Sphere center : " + baryCenter + " ; radius : " + meanRadius);
     }
 
-    private void OnDrawGizmos() {
-        if (_spheres.Count > 0) {
-            Gizmos.color = Color.green;
-            foreach (var sphere in _spheres) {
-                Gizmos.DrawWireSphere(sphere.Center, sphere.Radius);
-            }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        for (int i = _spheres.Count - 1; i >= 0; i--)
+        {
+            Gizmos.DrawWireSphere(_spheres[i].Center, _spheres[i].Radius);
         }
     }
 }
